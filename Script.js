2e9df907d68e3587c7bb22a18a80803f98db3c56
rust <53446488+lmssieh@users.noreply.github.com>
2022-09-20 23:57:14 +0100

    remove a lot of unneeded duplication

--- Script.js
+++ Script.js
@@ -22,70 +22,48 @@
 // @grant        none
 // @namespace https://greasyfork.org/users/703117
 // ==/UserScript==
-(function() {
-    'use strict';
- 
-var tracerColors = ""
-var func = {
-    webpack_inject3: (w, e, get) => {
-        tracerColors = get("ad1c4e70")
-    },
-};
-if(typeof window.webpackJsonp === 'function') {
-    window.webpackJsonp([0], func, ["webpack_inject3"]);
-} else {
-    window.webpackJsonp.push([
-        ["webpack_inject3"],
-        func,
-        [["webpack_inject3"]]
-    ]);
-}
-Object.keys(tracerColors).forEach(function(key52) {
-    if(key52.match(/mac10/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/Skins/mac10.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/glock|glock_dual/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/glock.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/mp5/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/mp5.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/ak/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/ak.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/scar/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/scar.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/sv98/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/sv98.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/bar/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/bar.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/m39/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/m39.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/spas12/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/spas12.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/m1014/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/m1014.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/l86/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/L86.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/scout_elite/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/scout.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/mk12/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/mk12.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/hk416/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/hk416.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    } else if(key52.match(/flare_gun/g)) {
-        tracerColors[key52].worldImg.sprite = "https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/guns-skin/flare.png";
-        tracerColors[key52].worldImg.tint = 0xFFFFFF
-    }
-})
+(function () {
+	"use strict";
+
+	var survivGuns = {};
+	var func = {
+		webpack_inject3: (w, e, get) => {
+			survivGuns = get("ad1c4e70");
+		},
+	};
+	if (typeof window.webpackJsonp === "function") {
+		window.webpackJsonp([0], func, ["webpack_inject3"]);
+	} else {
+		window.webpackJsonp.push([
+			["webpack_inject3"],
+			func,
+			[["webpack_inject3"]],
+		]);
+	}
+
+	const guns = [
+		"mac10",
+		"glock",
+		"mp5",
+		"ak",
+		"scar",
+		"sv98",
+		"bar",
+		"m39",
+		"spas12",
+		"m1014",
+		"l86",
+		"scout_elite",
+		"mk12",
+		"hk416",
+		"flare_gun",
+	];
+	const baseURL =
+		"https://raw.githubusercontent.com/iBLiSSIN/Surviv.io-Gun-Reskin/main/Skins/";
+	tracerColors[key52].worldImg.tint = 0xffffff;
+
+	guns.forEach(function (gun) {
+		survivGuns[gun].worldImg.sprite = `${baseURL}${gun}.png`;
+		survivGuns[gun].worldImg.tint = 0xffffff;
+	});
 })();
